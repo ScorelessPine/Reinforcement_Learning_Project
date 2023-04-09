cbe182fc3b7778f3838e01a11ffa8456ef26a4c3
Connor Swinney <ScorelessPine@gmail.com>
2023-04-09 15:58:09 -0700

    Update .gitignore. Complete Project 2 Self Driving.

---
+++ Project2SelfDriving.py
@@ -0,0 +1,46 @@
+import gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.evaluation import evaluate_policy
+import os
+
+environment_name = 'CarRacing-v0'
+env = gym.make(environment_name)
+log_path = os.path.join('Training','Logs')
+ppo_path = os.path.join('Training','Saved Models','PPO_Driving_Model')
+
+def test_environment():
+    
+    episodes = 5
+    for episode in range(1, episodes+1):
+        state = env.reset()
+        done = False
+        score = 0
+        
+        while not done:
+            env.render()
+            action = env.action_space.sample()
+            n_state, reward, done, info = env.step(action)
+            score += reward
+        print('Episode:{} Score:{}'.format(episode, score))
+    env.close()
+
+def train_model():
+    env=gym.make(environment_name)
+    env=DummyVecEnv([lambda: env])
+    model = PPO('CnnPolicy', env, verbose=1,tensorboard_log=log_path)
+    
+    model.learn(total_timesteps=100000)
+    model.save(ppo_path)
+
+def evaluate_and_test():
+    model = PPO.load(ppo_path, env)
+    evaluate_policy(model, env, n_eval_episodes=10, render=True)
+    env.close()
+
+if __name__ == '__main__':
+    #test_environment()
+    #train_model()
+    evaluate_and_test()
+    
+    
