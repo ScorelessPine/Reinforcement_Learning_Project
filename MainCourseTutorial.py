9369d7997aef1676077e52276790bc3819912e14
Connor Swinney <ScorelessPine@gmail.com>
2023-04-09 12:40:13 -0700

    Save and reload model

--- MainCourseTutorial.py
+++ MainCourseTutorial.py
@@ -1,9 +1,11 @@
+#====================================== 1. Import dependencies
 import os
 import gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 
+#====================================== 2. Load Environment
 environment_name = 'CartPole-v0'
 env = gym.make(environment_name)
 
@@ -21,6 +23,7 @@ for episode in range(1, episodes+1):
     print('Episode:{} Score:{}'.format(episode, score))
 env.close()
 
+#====================================== 3. Train an RL Model
 log_path = os.path.join('Training','Logs')
 
 env = gym.make(environment_name)
@@ -28,3 +31,10 @@ env = DummyVecEnv([lambda:env])
 model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=log_path)
 
 model.learn(total_timesteps=20000)
+
+#====================================== 4. Save and Reload Model
+PPO_Path = os.path.join('Training','Saved Models','PPO_Model_Cartpole')
+model.save(PPO_Path)
+del model
+model = PPO.load(PPO_Path, env=env)
+model.learn(total_timesteps=1000)
