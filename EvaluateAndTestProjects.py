07ca5309f966f1a960d6c8c70db670182ed85065
Connor Swinney <ScorelessPine@gmail.com>
2023-04-10 12:34:33 -0700

    Modify .gitignore. Add evaluation script.

---
+++ EvaluateAndTestProjects.py
@@ -0,0 +1,19 @@
+import Project1Breakout
+import Project2SelfDriving
+import Project3CustomEnvironment
+
+def main():
+    
+    print('===============================\nProject 1: Breakout\n===============================')
+    Project1Breakout.evaluate_and_test()
+    print('===============================\nProject 2: Self Driving\n===============================')
+    Project2SelfDriving.evaluate_and_test()
+    print('===============================\nProject 3: Custom Environment\n===============================')
+    Project3CustomEnvironment.evaluate_and_test()
+
+
+
+
+if __name__ == '__main__':
+    main()
+    
